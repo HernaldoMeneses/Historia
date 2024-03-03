791fba221b09915d37b767bb407401a0d15441cb
HernaldoMeneses <hernaldomeneses@gmail.com>
2024-03-03 16:47:06 -0300

    1-Varios-Caminhos.py

---
+++ 1-Bastidores/1-Varios-Caminhos.py
@@ -0,0 +1,34 @@
+#Bastidores da História
+##Estudar História
+
+Tema = "Conhecimento histórico"
+
+Tese = "Historia é Construída e Dinâmica ao longo do tempo"
+
+Sec_XIX = [
+    'A autonimia da História é Recente',
+    'Séc XIX - Baseava-se em documentos oficiais apenas',
+    'Séc XIX - Privilegiava-se os fatos políticos',
+    'Séc XIX - Voltavam-se principalmente para sustentação e a legitimação das nações emergentes (Nacionalismo)',
+    'Séc XIX - Enaltecia-se figuras políticas.'
+    ]
+
+Sec_XX = [
+    'Ampliação das fontes de pesquisas e no ensino da História'
+    'Novas Abordagens Temáticas e de pesquisas'
+    'Novas Horizontes e Pespectivas para os historiadores'
+    'Além dos documentos oficiais toda atividade humana passa a ser considerada fonte histórica'
+    'História Total'
+    'Conhecer História não é mais apenas memorizar fatos políticos e datas descritas'
+    'Conhecer História é a busca constante na compreensão do passado com base em indagações presentes'
+    'Conhecer História é a busca constante na compreensão das origens dos problemas presentes'
+    'Conhecer História é ainda a busca de soluções antigas com roupagens adequadas para problemas de hoje'
+    'Nesse intrelassar de presente e passado cuidados precisam ser tomados'
+    'Não podemos reduzir outros tempos e lugares aos olhares do presente'
+    'Historiadores possuem origem, formação cultural, classe social e religiões diversas'
+    'Diverso são os olhares e intepretações de fatos históricos'
+]
+
+
+Fontes_Historicas =[]
+#pag. 11/348
